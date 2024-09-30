ce73fb9b4ad431d228eb23a39c2081ebbd3c323f
faith <143903684+Faith-Kpogo@users.noreply.github.com>
2024-09-30 20:33:32 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
+$("#menu").click(function () {
+  $(".navbar").addClass("active");
+});
 
-$('#menu').click(function() {
-    $('.navbar').addClass('active')
-  });
+$("#close").click(function () {
+  $(".navbar").removeClass("active");
+});
 
-  $('#close').click(function() {
-    $('.navbar').removeClass('active')
-  });
-  
-
-  
+function moveTo() {
+  window.location.href = "product.html";
+}
